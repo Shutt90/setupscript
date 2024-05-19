4694c80a782eb00dfef82f20b62013e38ab1ba0a
Liam Pugh <liam.pugh.009@gmail.com>
2024-05-19 19:08:58 +0100

    fetch theme and run brew installs

--- main.go
+++ main.go
@@ -4,14 +4,33 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
+	"os/exec"
+	"strings"
 	"time"
 )
 
-func main() {
-	getHelixConfig()
+var stuffToInstall = []string{
+	"helix",
+	"postman",
+	"go",
+	"vue-language-server",
+	"typescript-language-server",
+	"vscode-css-language-server",
+	"intelephense",
+	"vscode-json-language-server",
+	"vscode-html-language-server",
+	"gopls",
+	"bash-language-server",
 }
 
-func getHelixConfig() string {
+func main() {
+	exec.Command("brew", "install", strings.Join(stuffToInstall, " "))
+	os.WriteFile("~/.config/helix/config.toml", getHelixConfig(), 0644)
+	os.WriteFile("~/.config/helix/themes/custom_theme.toml", getHelixTheme(), 0644)
+}
+
+func getHelixConfig() []byte {
 	c := http.Client{Timeout: time.Duration(30) * time.Second}
 	res, err := c.Get("https://raw.githubusercontent.com/Shutt90/archdotfiles/main/helix/config.toml")
 	if err != nil {
@@ -24,5 +43,21 @@ func getHelixConfig() string {
 		fmt.Println(err)
 	}
 
-	return string(body)
+	return body
+}
+
+func getHelixTheme() []byte {
+	c := http.Client{Timeout: time.Duration(30) * time.Second}
+	res, err := c.Get("https://raw.githubusercontent.com/Shutt90/archdotfiles/main/helix/themes/custom_theme.toml")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return body
 }
